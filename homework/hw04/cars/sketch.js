53ba20347335aa519deb05ac7a99b23ac4a2176e
Chris <christopherbolivar47@gmail.com>
2022-10-19 20:13:36 -0400

    Homework 4

--- homework/hw04/cars/sketch.js
+++ homework/hw04/cars/sketch.js
@@ -37,8 +37,8 @@ function draw() {
     drawCar(c1.x, c1.y, c1.width, c1.color);
     drawCar(c2.x, c2.y, c2.width, c2.color);
 
-    if (c1.x > 2000) {
-        c1.x = -100;
+    if (c1.x > 2200) {
+        c1.x = -200;
     }
 
     if (c2.x < -100) {
